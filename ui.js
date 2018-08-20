91f35ad6c2c8b5641e1e45875e042a30df71b6b6
andrey <volosgoto@yahoo.com>
2018-08-20 21:14:18 +0300

    refactor

--- ui.js
+++ ui.js
@@ -1,29 +1,48 @@
-class Ui{
-    constructor(){
+class Ui {
+    constructor() {
         this.location = document.getElementById('w-location');
         this.desc = document.getElementById('w-desc');
         this.string = document.getElementById('w-string');
+
+        // this.stringMax = document.getElementById('w-stringMax');
+        // this.stringMin = document.getElementById('w-stringMin');
+
         this.icon = document.getElementById('w-icon');
-        this.details = document.getElementById('w-details');
+
+        // this.details = document.getElementById('w-details');
+
         this.humidity = document.getElementById('w-humidity');
-        this.dewpoint = document.getElementById('w-dewpoint');
-        this.fellslike = document.getElementById('w-fells-like');
+        this.pressure = document.getElementById('w-pressure');
         this.wind = document.getElementById('w-wind');
     }
 
 
-    paint(weather){
+    paint(weather) {
+        //     // Wether location city
         this.location.textContent = weather.name;
-        this.desc.textContent = weather.weather[0].description;
-        //to Celsius
-        this.string.textContent = 'Temperature: ' + (weather.main.temp - 273).toFixed(1);    
-        this.icon.setAttribute('src', `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
-        
-        this.humidity.textContent = `Relative humidity: ${weather.main.humidity}`;
-        this.fellslike.textContent = 'Good';
-        this.dewpoint.textContent = 'Dewp oint: ' + '40';
-        this.wind.textContent = 'Wind speed: ' + weather.wind.speed + ' m/sec';
 
+        // Weather deskription
+        let wd = weather.weather[0].description;
+        this.desc.textContent = (function() {
+            return wd[0].toUpperCase() + wd.slice(1);
+        })(wd);
+
+        //to Celsius
+        this.string.textContent = 'Temperature: ' + (weather.main.temp - 273).toFixed(1);
+        // this.stringMax.textContent = 'Max: ' + (weather.main.temp_max - 273).toFixed(1);
+        // this.stringMin.textContent = 'Min: ' + (weather.main.temp_min - 273).toFixed(1);
+
+        // Pressure
+        let mmhg = 0.750062;
+        let pressureConvert = mmhg;
+        this.pressure.textContent = 'Pressure: ' + (weather.main.pressure * mmhg).toFixed(1) + ' mmhg';
+
+        // Add weather-icon
+        let imgUrl = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
+        this.icon.setAttribute('src', imgUrl);
+
+        this.humidity.textContent = `Relative humidity: ${weather.main.humidity}`;
+        this.wind.textContent = 'Wind speed: ' + weather.wind.speed + ' m/sec';
 
     }
 }
--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
-class Weather{
-    constructor(city, state){
+class Weather {
+    constructor(city, state) {
         // this.apiKey = '9a1df0044cfa9748';
         this.apiKey = 'eaa5a4f3e94da160e4967fc1c46a8a06';
         this.city = city;
@@ -8,7 +8,7 @@ class Weather{
 
 
     // Fetch wether from API
-    async getWeather(){
+    async getWeather() {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&APPID=${this.apiKey}`);
 
 
